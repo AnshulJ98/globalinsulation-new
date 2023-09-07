1666ccc21055cdf1f754bf23cb555a27c6c35742
Anshul Joshi <joshianshul98@gmail.com>
2023-09-07 16:30:43 -0400

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -7,5 +8,7 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [react()],
   site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+  base: '/astro-ecommerce', 
+  output:'server',
+  adapter:vercel()
 });
