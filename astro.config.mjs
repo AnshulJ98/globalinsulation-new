6c84389565792fd4ff1b0c4076af8b96c7552716
Anshul Joshi <joshianshul98@gmail.com>
2023-09-07 16:57:07 -0400

    remove ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -7,7 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
-  output:'server',
-  adapter:vercel()
+  base: '/astro-ecommerce'
 });
