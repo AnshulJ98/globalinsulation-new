899b5d77acc57a2837ecef1179c7fc6f15af91f4
Anshul Joshi <joshianshul98@gmail.com>
2023-09-07 17:08:19 -0400

    change url

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/astro-ecommerce/landing/">Global Insulation</a>
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="/landing/">Global Insulation</a>
         <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navigation">
           <ul className="navbar-nav ms-auto">
             {/* <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/">
                 All Components
               </a>
             </li> */}
@@ -22,9 +22,9 @@ const Navbar = () => {
                 Pages
               </a>
               <ul className="dropdown-menu" aria-labelledby="pagesExample">
-                <li><a className="dropdown-item" href="/astro-ecommerce/landing/">Home</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/product/">Product Page</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/shopping-cart/">Shopping Cart</a></li>
+                <li><a className="dropdown-item" href="/landing/">Home</a></li>
+                <li><a className="dropdown-item" href="/product/">Product Page</a></li>
+                <li><a className="dropdown-item" href="/shopping-cart/">Shopping Cart</a></li>
               </ul>
             </li> */}
             <li className="nav-item dropdown">
@@ -32,22 +32,22 @@ const Navbar = () => {
                 Brands
               </a>
               <ul className="dropdown-menu" aria-labelledby="pagesExample">
-                <li><a className="dropdown-item" href="/astro-ecommerce/ametalin/">Ametalin</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/csrbradford/">CSR Bradford</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/knaufearthwool/">Knauf Earthwool</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/fletcherpinkbatts/">Fletcher Pink Batts</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/foilboard/">Foilboard</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/jameshardiefire/">James Hardie Fire</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/kingspan/">Kingspan</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/sisalation/">Sisalation</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/polyestersolutions/">Polyester Solutions</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/pirmax/">PirMax</a></li>
-                <li><a className="dropdown-item" href="/astro-ecommerce/ecowool/">EcoWool</a></li>
-                {/* <li><a className="dropdown-item" href="/astro-ecommerce/shopping-cart/">Shopping Cart</a></li> */}
+                <li><a className="dropdown-item" href="/ametalin/">Ametalin</a></li>
+                <li><a className="dropdown-item" href="/csrbradford/">CSR Bradford</a></li>
+                <li><a className="dropdown-item" href="/knaufearthwool/">Knauf Earthwool</a></li>
+                <li><a className="dropdown-item" href="/fletcherpinkbatts/">Fletcher Pink Batts</a></li>
+                <li><a className="dropdown-item" href="/foilboard/">Foilboard</a></li>
+                <li><a className="dropdown-item" href="/jameshardiefire/">James Hardie Fire</a></li>
+                <li><a className="dropdown-item" href="/kingspan/">Kingspan</a></li>
+                <li><a className="dropdown-item" href="/sisalation/">Sisalation</a></li>
+                <li><a className="dropdown-item" href="/polyestersolutions/">Polyester Solutions</a></li>
+                <li><a className="dropdown-item" href="/pirmax/">PirMax</a></li>
+                <li><a className="dropdown-item" href="/ecowool/">EcoWool</a></li>
+                {/* <li><a className="dropdown-item" href="/shopping-cart/">Shopping Cart</a></li> */}
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/about-us/">
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/about-us/">
                 About Us
               </a>
             </li>
